94d25ba625a381598c51460d4e2bdd2f660ef176
drew <andrew.rattana@gmail.com>
2022-08-27 00:52:20 -0400

    add initial tests

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -2,4 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+
+/**
+ * @jest-environment jsdom
+ */
+import { setImmediate } from "timers"
+import "@testing-library/jest-dom"
+global.setImmediate = setImmediate
